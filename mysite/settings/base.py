4d8f804e0c99a161648ced3c9fa4ad510a75bdb7
octitesta <octis.testa@gmail.com>
2026-02-25 23:02:00 -0300

    Connect production to Neon PostgreSQL

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -14,18 +14,16 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-super-seguro")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -111,8 +109,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = True
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
---
+++ mysite/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+import os
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["apuestas-f1.onrender.com"]
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
